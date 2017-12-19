26bf6e8b0513aef8d25ae2a40342633d06d34066
Jakub Barszczewski <jakub.barszczewski@livenation.co.uk>
2017-12-19 15:53:36 +0000

    added simple styles

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,19 +1,26 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 
+import './Card.css';
+
 class Card extends React.PureComponent {
-	render() {
-		return (
-			<div>
-				{this.props.title} {this.props.release_date}
-			</div>
-		);
-	}
+    render() {
+        return (
+            <div className="Card">
+                <p className="Title">
+                    {this.props.title}
+                </p>
+                <p className="Year">
+                    {this.props.release_date}
+                </p>
+            </div>
+        );
+    }
 }
 
 Card.propTypes = {
     title: PropTypes.string,
     release_date: PropTypes.string
-        }
+}
 
 export default Card;
